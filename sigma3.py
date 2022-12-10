b4511fffabb5fc3da83ffef32dfc5e4513ae06d1
Neva <117465826+muneva@users.noreply.github.com>
2022-12-11 02:05:53 +0300

    Update sigma3.py

--- sigma3.py
+++ sigma3.py
@@ -1,5 +1,5 @@
 f = open("data.txt", mode='w', encoding='utf-8')
-f.write("Name: Munevver Neva\nSurname: Armagan\nGender: Female\nUsername: Buyuk Ev Ablukada\nJob: Artist\n")
+f.write("Name: Munevver Neva\nSurname: Armagan\nGender: Female\nUsername: Muneva\nJob: Student\n")
 f.close
 
 f = open("data.txt", mode='r', encoding='utf-8')
