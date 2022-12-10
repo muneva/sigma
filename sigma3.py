acff734cc0e332f16d1c01aadea7468ff95414b2
Neva <117465826+muneva@users.noreply.github.com>
2022-12-11 02:00:00 +0300

    Add files via upload

---
+++ sigma3.py
@@ -0,0 +1,10 @@
+f = open("data.txt", mode='w', encoding='utf-8')
+f.write("Name: Munevver Neva\nSurname: Armagan\nGender: Female\nUsername: Buyuk Ev Ablukada\nJob: Artist\n")
+f.close
+
+f = open("data.txt", mode='r', encoding='utf-8')
+d = {}
+for i in f:
+    liste = i.split(": ")
+    d[liste[0]] = liste[1][0:-1]
+print(d)
